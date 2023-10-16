3009da91190a25f7605ae1083f7228b43ab0bac1
Vasil Sariev <vasilsariev@Vasils-MacBook-Air.local>
2023-10-16 17:27:07 +0300

    first web3 code commit

--- README.md
+++ README.md
@@ -1,66 +1,63 @@
-## Foundry
+# FundMe Smart Contract
 
-**Foundry is a blazing fast, portable and modular toolkit for Ethereum application development written in Rust.**
+The `FundMe` contract provides a way for users to send funds and for the contract owner to withdraw them. Additionally, this contract uses the Chainlink price feed to ensure a minimum USD amount is funded by the users.
 
-Foundry consists of:
+## Features
 
--   **Forge**: Ethereum testing framework (like Truffle, Hardhat and DappTools).
--   **Cast**: Swiss army knife for interacting with EVM smart contracts, sending transactions and getting chain data.
--   **Anvil**: Local Ethereum node, akin to Ganache, Hardhat Network.
--   **Chisel**: Fast, utilitarian, and verbose solidity REPL.
+- Fund the contract with a minimum USD value.
+- Get the current version of the price feed.
+- Withdraw funds only by the owner.
+- Handle the direct ether sent to the contract address.
+- View functions for retrieving data about the funders and amounts.
 
-## Documentation
+## Dependencies
 
-https://book.getfoundry.sh/
+- Chainlink AggregatorV3Interface: For obtaining the latest ETH/USD conversion rate.
+- PriceConverter: A helper library to convert Ether to USD.
 
-## Usage
+## Contract Methods
 
-### Build
+### `fund()`
 
-```shell
-$ forge build
-```
+Allows a user to fund the contract. Ensures the minimum USD value is maintained.
 
-### Test
+### `getVersion()`
 
-```shell
-$ forge test
-```
+Returns the current version of the Chainlink price feed.
 
-### Format
+### `cheaperWithdraw()`
 
-```shell
-$ forge fmt
-```
+A method to withdraw funds efficiently, clearing the record of funders and their respective amounts. Only accessible by the owner.
 
-### Gas Snapshots
+### `withdraw()`
 
-```shell
-$ forge snapshot
-```
+Allows the owner to withdraw all funds from the contract.
 
-### Anvil
+### `fallback() & receive()`
 
-```shell
-$ anvil
-```
+Handles direct ether sent to the contract address.
 
-### Deploy
+### View Functions
 
-```shell
-$ forge script script/Counter.s.sol:CounterScript --rpc-url <your_rpc_url> --private-key <your_private_key>
-```
+- `getAddressToAmountFunded(address)`: Returns the amount funded by a specific address.
+- `getFunder(uint256)`: Returns the address of a funder based on their index.
+- `getFundersCount()`: Returns the total count of funders.
+- `getOwner()`: Returns the address of the owner.
 
-### Cast
+## Installation & Setup
 
-```shell
-$ cast <subcommand>
-```
+1. Install the Chainlink package:
+ ```
+ npm install @chainlink/contracts
+ ```
 
-### Help
+2. Deploy the `FundMe` contract by providing the address of the Chainlink price feed contract.
 
-```shell
-$ forge --help
-$ anvil --help
-$ cast --help
-```
+## Notes
+
+Make sure you handle fallback and receive with care, given that they automatically fund the contract.
+
+## License
+
+This project is licensed under the MIT License.
+
---
+++ test/unit/FundMeTest.t.sol
@@ -0,0 +1,154 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    uint256 constant GAS_PRICE = 1;
+
+    function setUp() external {
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        console.log(fundMe.getOwner());
+        console.log(msg.sender);
+        assertEq(fundMe.getOwner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        console.log(version);
+        assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // the next line should revert
+        fundMe.fund();
+        //fundMe.fund{value: 1e18}(); will revert
+    }
+
+    function testAddsFunderToArrayOfFunders() public funded {
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, USER);
+    }
+
+    function testFundUpdatesFundedDataStructure() public funded {
+        uint256 amountFunded = fundMe.getAddressToAmoundFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.expectRevert();
+        vm.prank(USER);
+        fundMe.withdraw();
+    }
+
+    function testWithDrawWithASingleFunder() public funded {
+        //Arange
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //Act
+        // uint256 gasStart = gasleft();
+        // vm.txGasPrice(GAS_PRICE);
+        vm.prank(fundMe.getOwner());
+        fundMe.withdraw();
+
+        // uint256 gasEnd = gasleft();
+        // uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
+        // console.log(gasUsed);
+
+        //Assert
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+    }
+
+    function testWithdrawFromMultipleFunders() public {
+        //Arrange
+        // to cast it to address  - if you want to generate addresses from numbers
+        uint160 numberOfFunders = 10;
+        // don't use address 0
+        uint160 startingFunderIndex = 1;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            // vm.prank new address
+            //vm.deal new address
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //Act
+        // the same as vm.prank but everything in between the lines is the prank
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        // Assert
+        assertEq(address(fundMe).balance, 0);
+        assertEq(
+            startingOwnerBalance + startingFundMeBalance,
+            fundMe.getOwner().balance
+        );
+    }
+
+    function testWithdrawFromMultipleFundersCheaper() public {
+        //Arrange
+        // to cast it to address  - if you want to generate addresses from numbers
+        uint160 numberOfFunders = 10;
+        // don't use address 0
+        uint160 startingFunderIndex = 1;
+
+        for (uint160 i = startingFunderIndex; i < numberOfFunders; i++) {
+            // vm.prank new address
+            //vm.deal new address
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+        uint256 startingFundMeBalance = address(fundMe).balance;
+
+        //Act
+        // the same as vm.prank but everything in between the lines is the prank
+        vm.startPrank(fundMe.getOwner());
+        fundMe.cheaperWithdraw();
+        vm.stopPrank();
+
+        // Assert
+        assertEq(address(fundMe).balance, 0);
+        assertEq(
+            startingOwnerBalance + startingFundMeBalance,
+            fundMe.getOwner().balance
+        );
+    }
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
+    }
+}
